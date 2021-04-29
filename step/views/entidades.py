58c77cb0468d3265b7c6052cea8c7537b6af47ea
dercio-sinione <derciosinione@gmail.com>
2021-04-29 19:00:19 +0100

    entidades

--- step/views/entidades.py
+++ step/views/entidades.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django import forms
+
+from step.models import Entidades
+
+
+class FormEntidade(forms.ModelForm):
+    class Meta:
+        model = Entidades
+        fields = ['nome', 'email', 'contacto',]
+
 
 @login_required
 def entidades(request):
-    return render(request, 'step/entidades.html',)
+    context = {
+        "result": Entidades.objects.all(),
+        }
+    return render(request, 'step/entidades.html', context)
+
+
+@login_required
+def addentidades(request):
+    if request.method == 'POST':
+        form = FormEntidade()
+        form.instance.user = request.user
+
+        # Validar o formulario
+        if form.is_valid():
+            form.save()
+            nome = form.cleaned_data.get('nome')
+            messages.success(
+                request, f'Entidade "{nome}" criado com sucesso!')
+            return redirect('step:entidades')
+        else:
+            messages.error(
+                request, f'{form.errors}', extra_tags='danger')
+    else:
+        form = FormEntidade()
+    return render(request, 'step/entidades.html', {'form': form})
