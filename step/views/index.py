d03ea7ce5056c846ef6431d6cd8f58bf0c733f1e
dercio-sinione <derciosinione@gmail.com>
2021-04-30 04:54:11 +0100

    gitIgnore

--- step/views/index.py
+++ step/views/index.py
@@ -2,14 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    # context = {
-    #     'actividades': Actividades.objects.order_by('-id').all()[:3],
-    # }
     return render(request, 'step/index.html',)
 
-
-def registar(request):
-    # context = {
-    #     'actividades': Actividades.objects.order_by('-id').all()[:3],
-    # }
-    return render(request, 'step/registar.html',)
